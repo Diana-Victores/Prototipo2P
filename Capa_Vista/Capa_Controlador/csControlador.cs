5814d4d831647ebd3a0c18417d1009a9e6db9683
Diana Victores <dcpernillo@gmail.com>
2022-09-27 14:22:24 -0600

    base de datos

--- Capa_Vista/Capa_Controlador/csControlador.cs
+++ Capa_Vista/Capa_Controlador/csControlador.cs
@@ -13,32 +13,32 @@ namespace Capa_Controlador
 {
     public class clscontrolador
     {
-    OdbcConnection con = new OdbcConnection("FIL=MS Acces;DSN=hotelsancarlos");
-    public bool InsertHabitacion (string _nomb, string _cons, string _area, string _camp, string text)
-    {
-        using (con)
+        OdbcConnection con = new OdbcConnection("FIL=MS Acces;DSN=hotelsancarlos");
+        public bool InsertHabitacion(string _nomb, string _cons, string _area, string _camp, string _stat)
         {
-            OdbcCommand cmd = new OdbcCommand();
-            con.Open();
-            cmd.Connection = con;
+            using (con)
+            {
+                OdbcCommand cmd = new OdbcCommand();
+                con.Open();
+                cmd.Connection = con;
 
-            #region Query
-            String query = @"INSERT INTO hotelsancarlos(idHabitacion,idTipoHabitacion,idTipoCama,precio) VALUE(?,?,?,?);";
-            #endregion
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = query;
+                #region Query
+                String query = @"INSERT INTO hotelsancarlos(idHabitacion, idTipoHabitacion, idTipoCama, precio, estatus) VALUE(?,?,?,?,?);";
+                #endregion
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = query;
 
-            cmd.Parameters.Add("@idHabitacion", OdbcType.VarChar).Value = _nomb;
-            cmd.Parameters.Add("@idTipoHabitacion", OdbcType.VarChar).Value = _cons;
-            cmd.Parameters.Add("@idTipoCama", OdbcType.VarChar).Value = _area;
-            cmd.Parameters.Add("@precio", OdbcType.VarChar).Value = _camp;
-            
+                cmd.Parameters.Add("@idHabitacion", OdbcType.VarChar).Value = _nomb;
+                cmd.Parameters.Add("@idTipoHabitacion", OdbcType.VarChar).Value = _cons;
+                cmd.Parameters.Add("@idTipoCama", OdbcType.VarChar).Value = _area;
+                cmd.Parameters.Add("@precio", OdbcType.VarChar).Value = _camp;
+                cmd.Parameters.Add("@estatus", OdbcType.VarChar).Value = _stat;
 
-            cmd.ExecuteNonQuery();
-            con.Close();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            return true;
         }
-        return true;
-    }
 
         public bool TipoHabitacion(string _a, string _b, string _c, string text)
         {
@@ -49,7 +49,7 @@ namespace Capa_Controlador
                 cmd.Connection = con;
 
                 #region Query
-                String query = @"INSERT INTO hotelsancarlos(idTipoHabitacion,nombre,estatus) VALUE(?,?,?);";
+                String query = @"INSERT INTO hotelsancarlos(idTipoHabitacion, nombre, estatus) VALUE(?,?,?);";
                 #endregion
                 cmd.CommandType = CommandType.Text;
                 cmd.CommandText = query;
@@ -57,7 +57,7 @@ namespace Capa_Controlador
                 cmd.Parameters.Add("@idTipoHabitacion", OdbcType.VarChar).Value = _a;
                 cmd.Parameters.Add("@nombre", OdbcType.VarChar).Value = _b;
                 cmd.Parameters.Add("@estatus", OdbcType.VarChar).Value = _c;
-               
+
 
 
                 cmd.ExecuteNonQuery();
@@ -66,8 +66,7 @@ namespace Capa_Controlador
             return true;
         }
 
-
-
+    
     }
 }
 
